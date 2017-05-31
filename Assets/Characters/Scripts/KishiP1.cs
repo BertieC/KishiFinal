10114bc5cd8588df2ac434a2d0d2409008132407
Karin <karin@karin.at>
2017-05-31 18:24:38 +0200

    animation scenes

---
+++ Assets/Characters/Scripts/KishiP1.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KishiP1 : MonoBehaviour {
+
+    public float kishiSpeed;
+    public bool isWalking;
+
+    public float walkTime;
+    public float idleTime;
+
+    public float walkCounter;
+    public float idleCounter;
+
+    private Rigidbody2D myRigitbody;
+    private Animation walkingAnimation;
+
+	// Use this for initialization
+	void Start () {
+        myRigitbody = GetComponent<Rigidbody2D>();
+        walkingAnimation = GetComponent<Animation>();
+
+        walkCounter = walkTime;
+        idleCounter = idleTime;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(isWalking == true)
+        {
+            
+            walkCounter -= Time.deltaTime;
+
+            myRigitbody.velocity = new Vector2(kishiSpeed, 0);
+
+            if (walkCounter < 0)
+            {
+                gameObject.GetComponent<Animator>().Play("Kishi_idle");
+                isWalking = false;
+                idleCounter = idleTime;
+            } 
+        }
+        else
+        {
+            idleCounter -= Time.deltaTime;
+            
+            myRigitbody.velocity = Vector2.zero;
+
+            if (idleCounter < 0)
+            {
+                gameObject.GetComponent<Animator>().Play("Kishi_walking");
+                isWalking = true;
+                walkCounter = walkTime;
+            }
+        }
+	}
+}
